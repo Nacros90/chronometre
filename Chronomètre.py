b55c79542b2df4c5b58ad6ea89302d5e4c734889
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-05-23 18:52:08 +0200

    Insertion d'un système de tours

--- Chronomètre.py
+++ Chronomètre.py
@@ -14,15 +14,25 @@ while start==1:
     print()
     print()
     stop=int(input("Pour arreter le chronometre taper un chiffre quelquonque puis appuyer sur \"enter\" : "))
-    if stop>=0 or stop <=9:
+    while stop==9:
+        tour=time.time()
+        nbtour=0
+        nbtour=nbtour+1
+        tour=tour-t1
+        tourS=truncate(tour,4)
+        tourmin=truncate(tourS/60,2)
+        print("Temps de tour n°",nbtour,":",tourS,"s ou",tourmin,"min.")           #injecter un système de renouvelement de tours
+        stop=int(input("Arreter le chronomètre : entre 0 à 8; lancer un nouveau tour : 9 : "))
+        nbtour=nbtour+1
+    if stop>=0 or stop <=8:
         t2=time.time()
         print("Arret du chronomètre...")
     t3=t2-t1
     t3s=truncate(t3,4)
     print()
-    print("Temps d\"execution du chronomètre en secondes :",t3s)
+    print("Temps d\"execution du chronomètre en secondes :",t3s,"s.")
     t3min=truncate((t3s/60),2)
-    print("Temps d\"execution du chronomètre en minutes :",t3min)
+    print("Temps d\"execution du chronomètre en minutes :",t3min,"min.")
     print()
     start=int(input("Voulez-vous lancer un nouveau chronometre ? (1.Oui; 2.Non) : "))
     t1=0
