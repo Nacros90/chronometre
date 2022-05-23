9499fa0724fa46773ceac2153581c4d5f1b2f63f
Nacros90 <91343291+Nacros90@users.noreply.github.com>
2022-05-23 22:18:26 +0200

    Réglage du problème de décompte de tours
    J'ai réglé le problème qui empêchait le décompte des tours, grâce à cela le chronomètre fonctionne parfaitement, il est juste impossible d'afficher le temps.

--- Chronomètre.py
+++ Chronomètre.py
@@ -13,16 +13,19 @@ while start==1:
     print("Début du chronomètre.")
     print()
     print()
-    stop=int(input("Pour arreter le chronometre taper un chiffre quelquonque puis appuyer sur \"enter\" : "))
-    while stop==9:
+    stop=int(input("Pour arreter le chronometre entrer un chiffre entre 0 et 8, pour enregistrer un tour entrer le chiffre 9 : "))
+    print()
+    nbtour=0
+    while stop==9:                                                          #système de tours
         tour=time.time()
-        nbtour=0
-        nbtour=nbtour+1
         tour=tour-t1
         tourS=truncate(tour,4)
         tourmin=truncate(tourS/60,2)
-        print("Temps de tour n°",nbtour,":",tourS,"s ou",tourmin,"min.")           #injecter un système de renouvelement de tours
-        stop=int(input("Arreter le chronomètre : entre 0 à 8; lancer un nouveau tour : 9 : "))
+        print("Tour n°",nbtour+1,":",tourS,"s ou",tourmin,"min.")
+        print()
+        print()
+        stop=int(input("Pour arreter le chronometre entrer un chiffre entre 0 et 8, pour enregistrer un nouveau tour entrer le chiffre 9 : "))
+        print()
         nbtour=nbtour+1
     if stop>=0 or stop <=8:
         t2=time.time()
@@ -30,9 +33,10 @@ while start==1:
     t3=t2-t1
     t3s=truncate(t3,4)
     print()
-    print("Temps d\"execution du chronomètre en secondes :",t3s,"s.")
+    print()
+    print("Temps d'execution du chronomètre en secondes :",t3s,"s.")
     t3min=truncate((t3s/60),2)
-    print("Temps d\"execution du chronomètre en minutes :",t3min,"min.")
+    print("Temps d'execution du chronomètre en minutes :",t3min,"min.")
     print()
     start=int(input("Voulez-vous lancer un nouveau chronometre ? (1.Oui; 2.Non) : "))
     t1=0
